922db799563fa42dca3db2244dac68f9e30e6d09
vbarnaaa <60694401+vbarnaaa@users.noreply.github.com>
2021-08-31 00:12:44 +0100

    data accessed
    Got the country and 90 days data and the countries

--- js/index.js
+++ js/index.js
@@ -1,29 +1,51 @@
-/*
-const port = 5500;
-
-//const cors = require('cors');
-//CREATING SERVER
-
-const express = require("express");
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-	res.render('home.ejs')
-})
-
-app.listen(port, () => {
-	console.log(`LISTENING ON PORT ${port}`);
-})
-*/
-
 		const countries = 'https://covid-api.com/api/regions?/';
+		const data = 'https://covid-api.com/api/reports?date=2020-04-16&iso=USA/'
+		const select = document.getElementById('countDrop');
+		//let days = [];
 
-		getData();
-		async function getData(){
+		for (let i = 0; i < 90; i++){
+			const date = new Date();
+			const ninety = date-86400000*i
+			//console.log(ninety)
+
+			function formatDate(ninety) {
+    			var d = new Date(ninety),
+        		month = '' + (d.getMonth() + 1),
+        		day = '' + d.getDate(),
+       			 year = d.getFullYear();
+
+    		if (month.length < 2) 
+        		month = '0' + month;
+    		if (day.length < 2) 
+       		 day = '0' + day;
+
+    		return [year, month, day].join('-');
+			}
+			//let days = new Array(formatDate(ninety))
+			console.log(formatDate(ninety));
+		}
+
+		getCountry();
+
+		async function getCountry(){
 			const response = await fetch(countries);
 			const data = await response.json();
-			const countryList = data.data;
-			console.log(countryList);
+				for (let i = 0; i < data.data.length; i++) {
+					//console.log(data.data[i].name);
+					const d = `<option value="${data.data[i].iso}">${data.data[i].name}</option>`;
+					const option = document.createElement('option');
+					option.setAttribute('value', `${data.data[i].iso}`);
+					option.innerText = (`${data.data[i].name}`);
+					select.appendChild(option);
+				}
 		}
+		function selectCountry() {
+
+		const selectedCountry = countDrop.options[countDrop.selectedIndex].value;
+		//for (let all of )
+
+		const newApi = `https://covid-api.com/api/reports?`
+		console.log(selectedCountry)
+		}
+
+
