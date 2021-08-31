2448bcf9ec8ecace6829261ddcd5c9c0dd0c55cb
vbarnaaa <60694401+vbarnaaa@users.noreply.github.com>
2021-08-31 21:20:29 +0100

    refurbishing the code
    added a date dropdown and submit button to access the data on deaths and n. of cases. on the selected date

--- js/index.js
+++ js/index.js
@@ -1,36 +1,26 @@
-		const countries = 'https://covid-api.com/api/regions?/';
-		//const data = 'https://covid-api.com/api/reports?date=2020-04-16&iso=USA/'
-		const select = document.getElementById('countDrop');
-		//let selectedCountry = 'US';
 		const x = [];
-		const y = []
-		let rv = {};
+		const deaths = [];
+		const active = [];
+		let newAPI = 'https://covid-api.com/api/reports?2021-08-31&iso=USA';
+		
+		
+
+/****************** GET THE DAYS OF THE LAST 90 DAYS *****************************/
 
 		for (let i = 0; i < 90; i++){
-			const date = new Date();
-			const ninety = date-86400000*i;
-			
-			function formatDate(ninety) {
-    			var d = new Date(ninety),
-        		month = '' + (d.getMonth() + 1),
-        		day = '' + d.getDate(),
-       			year = d.getFullYear();
-
-    		if (month.length < 2) 
-        		month = '0' + month;
-    		if (day.length < 2) 
-       		 day = '0' + day;
-
-    		return [year, month, day].join('-');
-
-			}
-			x.push(formatDate(ninety));
-			
-			//console.log(newApi);
+			const today = new Date();
+			const ninety = today-86400000*i;
+			const dates = dayjs(ninety).format('YYYY-MM-DD')
+			x.push(dates);
+			//console.log(dates)
+					const dateSelect = document.getElementById('dateDrop');
+					const option = document.createElement('option');
+					option.setAttribute('value', dates);
+					option.innerText = (dates);
+					dateSelect.appendChild(option);
 		}
 
-		console.log(x);
-		//console.log(rv);
+/****************** GET THE LIST OF COUNTRIES TO QUERY THE DATA ********************/
 		getCountry();
 
 		async function getCountry(){
@@ -38,6 +28,7 @@
 			const data = await response.json();
 				for (let i = 0; i < data.data.length; i++) {
 					//console.log(data.data[i].name);
+					const select = document.getElementById('countDrop');
 					const d = `<option value="${data.data[i].iso}">${data.data[i].name}</option>`;
 					const option = document.createElement('option');
 					option.setAttribute('value', `${data.data[i].iso}`);
@@ -45,62 +36,27 @@
 					select.appendChild(option);
 				}
 		}
+/****************** CONSTRACT NEW API (COUNTRY, DATE) ********************/
 		
-		async function selectCountry() {
+		
 
-		selectedCountry = countDrop.options[countDrop.selectedIndex].value;
+		function getNewAPI() {
 
-		for (let i = 0; i < x.length; i++){
-			const newApi = [(`https://covid-api.com/api/reports?${x[i]}&iso=${selectedCountry}`)]
-			
-			getY();
-			async function getY(){
-				const response = await fetch(newApi);
-			    const data = await response.json();
-			    y.push(data)
-			    //const arr = data[0];
-			    console.log(data)
-			    //data.active = v
-			    
-			    
+			const selectedCountry = countDrop.options[countDrop.selectedIndex].value;
+			const selectedDate = dateDrop.options[dateDrop.selectedIndex].value;
+			newAPI = (`https://covid-api.com/api/reports?${selectedDate}&iso=${selectedCountry}`);
+			console.log(newAPI);
+			getData();
+		async function getData() {
+				const response = await fetch(newAPI);
+				const data = await response.json();	
+				//deaths = data.deaths;
+				//const obj = JSON.parse(data);
+				console.log(data.data[0].deaths,
+							data.data[0].active);
+
+
 			}
-			//console.log(y)
-			
-
 		}
 		
 		
-		//console.log(selectedCountry)
-		
-		//return newApi;
-		} 
-		//.then(console.log(y))
-
-
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: x,
-        datasets: [{
-            label: '# of Votes',
-            data: y,
-            backgroundColor: [
- 
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
