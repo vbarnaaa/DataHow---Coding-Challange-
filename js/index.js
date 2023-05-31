25646ca1225500c00cf10ec9fbfa135a8904ed39
vbrnbs <varszegibarnabas@gmail.com>
2023-06-01 07:09:08 +1200

    CHANGED FROM DATE AND COUNTRY TO ONLY DATE

--- js/index.js
+++ js/index.js
@@ -1,120 +1,198 @@
-		const x = [];
-		let sumA = 0;
-		let sumD = 0;
-		let popp = 0;
-		let newAPI = 'https://covid-api.com/api/reports?2021-08-31&iso=USA';
+const x = [];
+// let sumA = 0;
+// let sumD = 0;
+// let popp = 0;
+let newAPI = '';
 
-		
-		/****************** GET THE DAYS OF THE LAST 90 DAYS *****************************/
+let iso = 'US';
+let country = 'USA';
+let rawCountryList = [];
 
-		for (let i = 0; i < 1000; i++){
-			const today = new Date();
-			const ninety = today-86400000*i;
-			const dates = dayjs(ninety).format('YYYY-MM-DD')
-			x.push(dates);
-			//console.log(dates)
-			const dateSelect = document.getElementById('dateDrop');
+let activeCases, ddeaths, population, flag;
+let activePercent, deathsPercent, popp;
+
+const countDrop = document.getElementById('countDrop');
+// const dateDrop = document.getElementById('dateDrop');
+
+
+/****************** GET THE DAYS OF THE LAST 90 DAYS *****************************/
+
+// for (let i = 200; i < 1000; i++) {
+// 	const today = new Date(2022, 0, 26);
+// 	const ninety = today - 86400000 * i;
+// 	const dates = dayjs(ninety).format('YYYY-MM-DD')
+// 	x.push(dates);
+// 	const option = document.createElement('option');
+// 	option.setAttribute('value', dates);
+// 	option.innerText = (dates);
+// 	dateDrop.appendChild(option);
+// }
+
+/****************** GET THE LIST OF COUNTRIES TO QUERY THE DATA ********************/
+getCountry();
+// getNewAPI("US");
+getUserCountry(console.log)
+function getUserCountry(callback) {
+	// Create a script element to load the API
+	var script = document.createElement('script');
+	script.src = 'https://ipvigilante.com/jsonp/?callback=getUserCountryDetails';
+  
+	// Append the script element to the document
+	document.getElementsByTagName('head')[0].appendChild(script);
+  
+	// Define a global callback function to process the API response
+	window.getUserCountryDetails = function(details) {
+	  // Return the country code to the original callback function
+	  callback(details.data.country_code);
+	  
+	  // Remove the script element from the document
+	  document.getElementsByTagName('head')[0].removeChild(script);
+	};
+  }
+  
+  // Example usage:
+  getUserCountry(countryCode => {
+	console.log('The user is in ' + countryCode);
+  });
+  
+  
+  // Example usage:
+  getUserCountry(countryCode => {
+	console.log('The user is in ' + countryCode);
+  });
+  
+async function getCountry() {
+	const url = 'https://countries-cities.p.rapidapi.com/location/country/list';
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '971dd3145bmsha650a70d9fd1399p123526jsnf76245687e21',
+			'X-RapidAPI-Host': 'countries-cities.p.rapidapi.com'
+		}
+	};
+
+	try {
+		const response = await fetch(url, options);
+		const data = await response.json();
+		const countryList = Object.values(data.countries).sort();
+		rawCountryList = data.countries;
+		const isoList = Object.keys(data.countries);
+		// console.log('countryList', rawCountryList);
+
+		for (let i = 0; i < countryList.length; i++) {
+			// const select = document.getElementById('countDrop');
+			const d = `<option value="${countryList[i]}">${countryList[i]}</option>`;
 			const option = document.createElement('option');
-			option.setAttribute('value', dates);
-			option.innerText = (dates);
-			dateSelect.appendChild(option);
+			option.setAttribute('value', `${countryList[i]}`);
+			option.innerText = (`${countryList[i]}`);
+			countDrop.appendChild(option);
 		}
 
-		/****************** GET THE LIST OF COUNTRIES TO QUERY THE DATA ********************/
-		getCountry();
+	} catch (error) {
+		console.error(error);
+	}
+}
+/****************** CONSTRACT NEW API (COUNTRY, DATE, POPULATION) ********************/
 
-		async function getCountry(){
-			const response = await fetch("https://covid-193.p.rapidapi.com/countries", {
-				"method": "GET",
-				"headers": {
-					"x-rapidapi-host": "covid-193.p.rapidapi.com",
-					"x-rapidapi-key": "971dd3145bmsha650a70d9fd1399p123526jsnf76245687e21"
-				}
-			});
-			const data = await response.json();
-			console.log(data.response);
-			for (let i = 0; i < data.response.length; i++) {
-					//console.log(data.data[i].name);
-					const select = document.getElementById('countDrop');
-					const d = `<option value="${data.response[i]}">${data.response[i]}</option>`;
-					const option = document.createElement('option');
-					option.setAttribute('value', `${data.response[i]}`);
-					option.innerText = (`${data.response[i]}`);
-					select.appendChild(option);
-				}
-			}
-			/****************** CONSTRACT NEW API (COUNTRY, DATE, POPULATION) ********************/
+function sumArray(array) {
+	let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
 
-			function getNewAPI() {
-
-				const selectedCountry = countDrop.options[countDrop.selectedIndex].value;
-				const selectedDate = dateDrop.options[dateDrop.selectedIndex].value;
-				newAPI = (`https://covid-193.p.rapidapi.com/history?country=${selectedCountry}&day=${selectedDate}`);
-				console.log(newAPI)
-
-				goo();
-					async function goo() {
-						const response = await fetch(newAPI, {
-							"method": "GET",
-							"headers": {
-								"x-rapidapi-host": "covid-193.p.rapidapi.com",
-								"x-rapidapi-key": "971dd3145bmsha650a70d9fd1399p123526jsnf76245687e21"
-							}
-						});
-						const data = await response.json();	
-						let dths = [];
-						let act = [];
-						console.log(data)
-					
-						let morto = [];
-						let actuato = []
-						// console.log(dths)
-						for (let i = 0; i < data.response.length; i++) {
-							act.push(data.response[i].cases);
-							dths.push(data.response[i].deaths);
-							let dd = Object.values(dths[i]);
-							let aa = Object.values(act[i]);
-							morto.push(parseInt(dd[1]))
-							actuato.push(parseInt(aa[4]))
-						}
-						console.log(actuato, morto)
-						sumD = Math.round((morto.reduce((a, b) => a + b, 0) / morto.length));
-						sumA = Math.round((actuato.reduce((a, b) => a + b, 0) / actuato.length));
-						popp = 1000000 - sumA - sumD;
-						console.log(popp, sumA, sumD)
-						let activeCases;
-						let deathsTD;
-						let popul;
-						if (data.response.length === 0) {
-							activeCases = "'no data'";
-							deathsTD = "'no data'";
-							popul = "'no data'";
-						} else {
-							activeCases = `${sumA.toLocaleString()}  / 1 million`;
-							deathsTD = `${sumD.toLocaleString()}  / 1 million`;
-							popul = data.response[0].population.toLocaleString();
-						}
-				isOn = true;
-				document.getElementById('active').innerText = (`Active Cases: ${activeCases}`);
-				document.getElementById('deaths').innerText = (`Deaths that day: ${deathsTD}`);
-				document.getElementById('popul').innerText = (`Total Population: ${popul}`);
-			}
-
+function getISO(countryName) {
+	for (const code in rawCountryList) {
+		if (rawCountryList.hasOwnProperty(code) && rawCountryList[code] === countryName) {
+			return code;
 		}
+	}
+	return null;
+}
 
-		/**************************************** NAVIGATION BAR **********************************/
+async function getPopulation(country) {
 
-		function openNav() {
-			document.querySelector(".click-text").style.opacity = "0";
-			document.getElementById("mySidenav").style.width = "100%";
-			document.getElementById("mySidenav").style.height = "100%";
+	const url = `https://countries-cities.p.rapidapi.com/location/country/${country}`;
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '971dd3145bmsha650a70d9fd1399p123526jsnf76245687e21',
+			'X-RapidAPI-Host': 'countries-cities.p.rapidapi.com'
 		}
+	};
 
-		function closeNav() {
-			document.getElementById("mySidenav").style.width = "0";
-		}
+	try {
+		const response = await fetch(url, options);
+		const result = await response.json();
+		console.log('population', result.population)
+		population = result.population;
+		flag = result.flag.emoji;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+function getNewAPI(country) {
+	// let newAPI = 'https://covid-api.com/api/reports?2021-08-31&iso=USA';
+	let newAPI = `https://covid-api.com/api/reports?2023-03-09&region_name=${country}`
+	getData(newAPI)
+	console.log('newAPI', newAPI)
+
+	async function getData(api) {
+		const response = await fetch(api);
+		const data = await response.json();
+		const iso = data.data.map((item) => item.region.iso)[0].slice(0, 2);
+
+		activeCases = sumArray(data.data.map((item) => item.active));
+		ddeaths = sumArray(data.data.map((item) => item.deaths));
+		population = getPopulation(iso);
+
+		// console.log('data: ', data)
+		console.log('data.data', data.data)
+		// console.log("mapped: ", data.data.map((item) => item))
+		// console.log("ISO: ", data.data.map((item) => item.region.iso)[0])
+	}
+	console.log('activeCases', activeCases, 'ddeaths', ddeaths, 'population', population);
+
+}
+
+function handleClickSubmit() {
+	// let selectedDate = dateDrop.options[dateDrop.selectedIndex].value;
+	let selectedCountry = countDrop.options[countDrop.selectedIndex].value;
+	getNewAPI(selectedCountry);
+	let selectedIso = getISO(selectedCountry)
+	getPopulation(selectedIso);
+	isOn = true;
+	// const popPerThousand = population / 1000;
+	activePercent = (activeCases / population) * 1000 ;
+	deathsPercent = (ddeaths / population) * 1000 ;
+	popp = 1000 - activePercent - deathsPercent;
+	console.log('activePercent', activePercent, 'deathsPercent', deathsPercent, 'popp', popp);
+	printData();
+}
+
+function printData() {
+document.getElementById('flag').innerText = (`${flag}`);
+document.getElementById('active').innerText = (`Active Cases: ${activeCases}`);
+document.getElementById('deaths').innerText = (`Deaths that day: ${ddeaths}`);
+document.getElementById('popul').innerText = (`Total Population: ${population}`);
+}
+
+
+/**************************************** NAVIGATION BAR **********************************/
+
+function openNav() {
+	document.querySelector(".click-text").style.opacity = "0";
+	document.getElementById("mySidenav").style.width = "100%";
+	document.getElementById("mySidenav").style.height = "100%";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
 
 
 
 
-		
+
--- js/sketch.js
+++ js/sketch.js
@@ -30,16 +30,15 @@ function draw() {
 /******** DELETING OLD AND ADDING NEW PARTICLES WHEN 'SUBMIT' CLICKED ***************************/
 
     if (isOn === true) {
-        
         particles = [];active = [];deaths = []; // clear the array of previous particles
 
-        for ( let i = 0; i < sumA/1000; i++){    // creating an array of 'new cases' with number of new cases divided by 100
+        for ( let i = 0; i < activePercent*2; i++){    // creating an array of 'new cases' with number of new cases divided by 100
           active.push(new Active());
         }
-        for (let i = 0; i < popp/1000; i++) { 
+        for (let i = 0; i < popp*2; i++) { 
           particles.push(new Particle());       // creating an array of 'populations' with number of country total population divided by 500 000
         }
-        for (let i = 0; i < sumD/1000; i++) {
+        for (let i = 0; i < deathsPercent*2; i++) {
           deaths.push(new Deaths());            // creating an array of 'deaths' with the number of new deaths
         }
         isOn = false;                           // disable function after arrays are generated
